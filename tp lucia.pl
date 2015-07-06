29a40f92d86cd73483c7963c116f9108cdd637f0
LuciaB2 <lucyutn@gmail.com>
2015-07-05 21:34:35 -0300

    actualizacion

--- tp lucia.pl
+++ tp lucia.pl
@@ -20,28 +20,42 @@ peso(simba, 200).
 peso(scar, 300).
 peso(shenzi, 400).
 peso(banzai, 500).
+peso(Animal,Peso):-
+	vaquitaSanAntonio(Animal,Peso).
+peso(Animal,Peso):-
+	cucaracha(Animal,_,Peso).
+peso(Animal,Peso):-
+	hormiga(Animal),
+	pesoHormiga(Peso).
 persigue(scar, timon).
 persigue(scar, pumba).
 persigue(shenzi, simba).
 persigue(shenzi, scar).
 persigue(banzai, timon).
 persigue(scar, mufasa).
-animal(mufasa).
-animal(scar).
-animal(timon).
-animal(pumba).
-animal(shenzi).
-animal(simba).
-animal(banzai).
+animal(Animal):-
+	peso(Animal,_).
+animal(Animal):-
+	persigue(_,Animal).
 
-cucaracha(NombreCucaracha,TamanioCucaracha,PesoCucaracha):-
-	 comio(_,cucaracha(NombreCucaracha,TamanioCucaracha,PesoCucaracha)).
+bicho(Animal):-
+	hormiga(Animal).
+bicho(Animal):-
+	vaquitaSanAntonio(Animal,_).
+bicho(Animal):-
+	cucaracha(Animal,_,_).
 
-hormiga(NombreHormiga):-
-	comio(_,hormiga(NombreHormiga)).
+hormiga(Animal):-
+	comio(_,hormiga(Animal)).
+vaquitaSanAntonio(Animal,Peso):-
+	comio(_, vaquitaSanAntonio(Animal,Peso)).
+cucaracha(Animal,TamanioCucaracha,PesoCucaracha):-
+	 comio(_,cucaracha(Animal,TamanioCucaracha,PesoCucaracha)).
+come(Animal,Comida) :- comio(Animal,vaquitaSanAntonio(Comida,_)).
+come(Animal,Comida) :- comio(Animal,hormiga(Comida)).
+come(Animal,Comida) :- comio(Animal,cucaracha(Comida,_,_)).
+come(Animal,Comida) :- persigue(Animal,Comida).
 
-vaquitaSanAntonio(NombreVaquita,PesoVaquita):-
-	 comio(_,vaquitaSanAntonio(NombreVaquita,PesoVaquita)).
 %1)a)
 jugosita(cucaracha(NombreCucaracha, TamanioCucaracha, PesoCucaracha)):-
  cucaracha(NombreCucaracha,TamanioCucaracha,PesoCucaracha),
@@ -54,24 +68,23 @@ hormigofilico(Personaje):-
 	hormigasQueComio(Personaje,Hormigas),
 	comioMasDeDosHormigas(Hormigas).
 
+
 hormigasQueComio(Personaje,Hormigas):-
 	comio(Personaje,_),
-	findall(NombreHormiga, comio(Personaje,hormiga(NombreHormiga)),Hormigas).
+	findall(NombreHormiga, (comio(Personaje,hormiga(NombreHormiga))),Hormigas).
 
 
 comioMasDeDosHormigas(Hormigas):-
 	length(Hormigas, CantidadHormigas),
 	CantidadHormigas >= 2.
 
- /*Ver de filtrar los resultados*/
-
-
 %c)
 cucarachofobico(Personaje):-
 	comio(Personaje,_),
-	not(comio(Personaje,cucaracha(_,_,_))).
-/*Ver de filtrar los resultados*/
+	noComioCucarachas(Personaje).
 
+noComioCucarachas(Personaje):-
+	not(comio(Personaje,cucaracha(_,_,_))).
 
 %d)
 picaron(pumba).
@@ -87,215 +100,112 @@ comioCucarachaJugosita(Personaje):-
 picarones(ListaDePersonajes):-
 	findall(Personaje, picaron(Personaje), ListaDePersonajes).
 
-%2) a)
+%2%a)
+cuantoEngorda(Animal, PesoTotal):-
+	animal(Animal),
+	findall(Peso, engorda(Animal,Peso),Pesos),
+	sumlist(Pesos,PesoTotal).
 
-cuantoEngorda(Personaje, Peso):-
-        comio(Personaje,_),
-	pesosDeBichosQueComio(Personaje, Peso).
+engorda(Animal, Peso):-
+	bicho(Bicho),
+	come(Animal, Bicho),
+	peso(Bicho,Peso).
 
-/*Ver que cuantoEngorda,  traigan resultados filtrados */
+%b)
+engordaPersecutor(Animal, Peso):-
+	come(Animal, Victima),
+	peso(Victima,Peso).
 
+cuantoEngordaPersecutor(Animal,PesoTotal):-
+	animal(Animal),
+	findall(Peso, engordaPersecutor(Animal,Peso),Pesos),
+	sumlist(Pesos,PesoTotal).
 
-pesosDeBichosQueComio(Personaje, 0):-
-	not(bichoQueComio(Personaje,_)).
-
-pesosDeBichosQueComio(Personaje, Peso):-
-	findall(PesoBicho, bichoQueComio(Personaje, PesoBicho), PesosBichos),
-	sumlist(PesosBichos, Peso).
-
-
-bichoQueComio(Personaje, PesoBicho):-
-	comio(Personaje,cucaracha(_,_,PesoBicho)).
-
-bichoQueComio(Personaje, PesoBicho):-
-	comio(Personaje,vaquitaSanAntonio(_,PesoBicho)).
-
-bichoQueComio(Personaje, PesoBicho):-
-	comio(Personaje,hormiga(_)),
-	pesoHormiga(PesoBicho).
-
-
-cuantoEngordaPersecusor(Personaje, Peso):-
-	pesoDeLasPrezasQuePersiguio(Personaje, PesoPrezas),
-	pesosDeBichosQueComio(Personaje, PesoBicho),
-	Peso is PesoPrezas + PesoBicho.
-
-pesoDeLasPrezasQuePersiguio(Personaje, 0):-
-	not(persigue(Personaje,_)).
-
-pesoDeLasPrezasQuePersiguio(Personaje, PesoPrezas):-
-	findall(PesoPreza, (persigue(Personaje, Preza),peso(Preza, PesoPreza)),ListaPesoPrezas),
-	sumlist(ListaPesoPrezas, PesoPrezas).
+%c)
+engordaVictima(Animal, 0):-
+	bicho(Animal).
+engordaVictima(Victima, Peso):-
+	bicho(Bicho),
+	come(Victima, Bicho),
+	peso(Bicho, Peso).
+engordaVictima(Victima, PesoV):-
+	bicho(bicho),
+	come(Victima, Bicho),
+	peso(Bicho,Peso),
+	findall(Peso, engordaVictima(Victima, Peso), Pesos),
+	sumlist(Pesos, PesoV).
 
 
-/*Ver que cuanto engorda final haga bien las cuentas*/
-
-
-
-cuantoEngordaFinal(Personaje,Peso):-
-	pesoFinalDeLasPrezasQuePersiguio(Personaje, PesoPrezas),
-	pesosDeBichosQueComio(Personaje, PesoBicho),
-	Peso is PesoPrezas + PesoBicho.
-
-pesoFinalDeLasPrezasQuePersiguio(Personaje, PesoPrezas):-
-	findall(PesoFinalPreza, (persigue(Personaje, Preza),pesoPrezaDespuesDeComer(Preza,PesoFinalPreza)),ListaPesoPrezas),
-	sumlist(ListaPesoPrezas, PesoPrezas).
-
-pesoPrezaDespuesDeComer(Preza,PesoFinalPreza):-
-	cuantoEngorda(Preza, Peso),
-	peso(Preza, PesoPreza),
-	PesoFinalPreza is PesoPreza + Peso.
-
+cuantoEngordaPersecutorPaciente(Animal, 0):-
+	bicho(Animal).
+cuantoEngordaPersecutorPaciente(Animal,PesoTotal):-
+	animal(Animal),
+	findall(Peso, (come(Animal, Victima), engordaVictima(Victima,Peso)),Pesos),
+	sumlist(Pesos,PesoTotal).
 
 %3
+perseguidoPorUno(Animal) :-
+	perseguidoPor(Animal,Perseguidores),
+	length(Perseguidores, 1).
 
+perseguidoPor(Animal,Perseguidores) :-
+	animalPerseguido(Animal),
+	findall( Perseguidor, (animal(Animal),animal(Perseguidor),persigue(Perseguidor,Animal)) ,Perseguidores).
 
-
-loPersigueSoloUno(Rey):-
-        animal(Rey),
-	findall(Persecusor, persigue(Persecusor, Rey), Persecusores),
-	length(Persecusores, 1).
-/*OK */
+animalPerseguido(Animal) :- persigue(_,Animal).
 
 adora(Animal, Rey):-
 	noLoPersigue(Rey, Animal).
-
 adora(Animal, Rey):-
 	noLoCome(Rey, Animal).
-
 noLoCome(Rey,Animal):-
+	animal(Animal),
+	animal(Rey),
 	not(come(Rey,Animal)).
-come(Rey, Animal):-
-	persigue(Rey, Animal).
-
 noLoPersigue(Rey, Animal):-
 	animal(Rey),
 	not(persigue(Rey, Animal)),
 	Rey \= Animal.
+rey(Rey) :-
+	perseguidoPorUno(Rey),
+	forall(animal(Animal),adora(Animal,Rey)).
 
-
-
-todosLoAdoran(Rey):-
-	animal(Rey),
-	forall((animal(Animal)), adora(Animal, Rey)).
-
-rey(Rey):-
-	loPersigueSoloUno(Rey),
-	todosLoAdoran(Rey).
-/*Aparece mas de 1 resultado
-debe aparecer solo mufasa
-
-elRey(Animal) :-
-     todosLoAdoran(Animal),
-     esPerseguidoPorLista(Animal,[Persecusor]).
-*/
-
-
-/*Enunciado
-Viscosos pero Sabrosos
-En la jungla tan imponente el león rey duerme ya... Y Timón y Pumba
-salieron a lastrar bichos.
-Tenemos tres tipos de bichos, representados por functores: las
-vaquitas de San Antonio (de quienes nos interesa un peso), las
-cucarachas (de quienes nos interesa un tamaño y un peso) y las
-hormigas, que pesan siempre lo mismo. De los personajes también se
-conoce el peso, mediante hechos.
-La base de conocimiento es la que sigue:
-%comio(Personaje, Bicho)
-comio(pumba, vaquitaSanAntonio(gervasia,3)).
-comio(pumba, hormiga(federica)).
-comio(pumba, hormiga(tuNoEresLaReina)).
-comio(pumba, cucaracha(ginger,15,6)).
-comio(pumba, cucaracha(erikElRojo,25,70)).
-comio(timon, vaquitaSanAntonio(romualda,4)).
-comio(timon, cucaracha(gimeno,12,8)).
-comio(timon, cucaracha(cucurucha,12,5)).
-comio(simba, vaquitaSanAntonio(remeditos,4)).
-comio(simba, hormiga(schwartzenegger)).
-comio(simba, hormiga(niato)).
-comio(simba, hormiga(lula)).
-pesoHormiga(2).
-%peso(Personaje, Peso)
-peso(pumba, 100).
-peso(timon, 50).
-peso(simba, 200).
-1) A falta de pochoclos…
-UTN FRBA - Paradigmas de Programación - Prof. Lucas Spigariol
-Parcial de Lógico - Viscosos Pero Sabrosos - Lunes 04/10/2010
-Definir los predicados que permitan saber:
-a) Qué cucaracha es jugosita: ó sea, hay otra con su mismo tamaño pero ella es más gordita.
-?- jugosita(cucaracha(gimeno,12,8)).
-Yes
-b) Si un personaje es hormigofílico... (Comió al menos dos hormigas).
-?- hormigofilico(X).
-X = pumba;
-X = simba.
-c) Si un personaje es cucarachofóbico (no comió cucarachas).
-?- cucarachofobico(X).
-X = simba
-d) Conocer al conjunto de los picarones. Un personaje es picarón si comió una cucaracha jugosita ó si se
-come a Remeditos la vaquita. Además, pumba es picarón de por sí.
-?- picarones(L).
-L = [pumba, timon, simba]
-2) Pero yo quiero carne…
-Aparece en escena el malvado Scar, que persigue a algunos de nuestros amigos. Y a su vez, las hienas Shenzi
-y Banzai también se divierten...
-persigue(scar, timon).
-persigue(scar, pumba).
-persigue(shenzi, simba).
-persigue(shenzi, scar).
-persigue(banzai, timon)
-Por ejemplo, un día había una hiena distraida y con mucho
-hambre y amplio su dieta
-comio(shenzi,hormiga(conCaraDeSimba)).
-Completando la base...
-peso(scar, 300).
-peso(shenzi, 400).
-peso(banzai, 500).
-a) Se quiere saber cuánto engorda un personaje (sabiendo que engorda una cantidad igual a la suma de
-los pesos de todos los bichos en su menú). Los bichos no engordan.
-?- cuantoEngorda(Personaje, Peso).
-Personaje= pumba
-Peso = 83;
-Personaje= timon
-Peso = 17;
-UTN FRBA - Paradigmas de Programación - Prof. Lucas Spigariol
-Parcial de Lógico - Viscosos Pero Sabrosos - Lunes 04/10/2010
-Personaje= simba
-Peso = 10
-b) Pero como indica la ley de la selva, cuando un personaje persigue a otro, se lo termina comiendo, y por lo
-tanto también engorda. Realizar una nueva version del predicado cuantoEngorda.
-?- cuantoEngorda(scar,Peso).
-Peso = 150
-(es la suma de lo que pesan pumba y timon)
-?- cuantoEngorda(shenzi,Peso).
-Peso = 502
-(es la suma del peso de scar y simba, mas 2 que pesa la hormiga)
-c) Ahora se complica el asunto, porque en realidad cada animal antes de comerse a sus víctimas espera a que
-estas se alimenten. De esta manera, lo que engorda un animal no es sólo el peso original de sus víctimas, sino
-también hay que tener en cuenta lo que éstas comieron y por lo tanto engordaron. Hacer una última version del
-predicado.
-?- cuantoEngorda(scar,Peso).
-Peso = 250
-(150, que era la suma de lo que pesan pumba y timon, más 83 que se come
-pumba y 17 que come timon )
-?- cuantoEngorda(shenzi,Peso).
-Peso = 762
-(502 era la suma del peso de scar y simba, mas 2 de la hormiga. A eso se
-le suman los 250 de todo lo que engorda scar y 10 que engorda simba)
-3) Buscando el rey…
-Sabiendo que todo animal adora a todo lo que no se lo come o no lo
-persigue, encontrar al rey. El rey es el animal a quien sólo hay un animal
-que lo persigue y todos adoran.
-Si se agrega el hecho:
-persigue(scar, mufasa).
-?- rey(R).
-R = mufasa.
-(sólo lo persigue scar y todos los adoran)
-4) Explicar en dónde se usaron y cómo fueron de utilidad los siguientes
-conceptos:
+/* 4
 a. Polimorfismo
+  En el punto 2, se usa "Victima", tanto para referirnos a un functor
+  bicho, como al nombre de una animal, sin tenes que tratar cada caso
+  por separado (animal o bicho)
+
 b. Recursividad
+  se usa en engordaVictima, ya que hay que saber cuanto emgorda cada una
+  de las victimas del animal que estamos consultando.
+
 c. Inversibilidad
+  Se utiliza en varios predicados, con la intencion de poder realizar
+  consultas para cada uno de los parametros. En algunos casos, fue
+  necesario generar algunas de las variables para lograr la
+  inversibilidad.
+  Alguno de los predicados que son inversibles con cucarachofobico,
+  jugosita, hormigofilico,  los "cuantoEngordan" del punto 2, etc
 
 */
+
+sinRepetidos([X], [X]).
+sinRepetidos(Lista, ListaNueva):-
+	findall((member(E,Lista), member(E2, Lista)), E \= E2, ListaNueva).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
